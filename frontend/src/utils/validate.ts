54a773669aabbdb6b2b54dfbaaa7b05ada2a28fd
ricardopitubasouza <ricardo_ptb@hotmail.com>
2022-04-25 21:54:08 -0300

    Save, useNavigate

---
+++ frontend/src/utils/validate.ts
@@ -0,0 +1,7 @@
+export function validateEmail(email: any) {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+[^<>()\.,;:\s@\"]{2,})$/
+      );
+  }
